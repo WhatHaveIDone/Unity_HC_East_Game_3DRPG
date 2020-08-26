74033b08fd47b324b1023740c48c2e4ccb7c9abe
WhatHaveIDone <37175348+WhatHaveIDone@users.noreply.github.com>
2020-08-26 21:39:46 +0800

    0826

---
+++ Assets/Scripts/LearnCoroutine.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;   // 使用'協程'時必須引用此 API(系統.集合)
+using UnityEngine;
+
+public class LearnCoroutine : MonoBehaviour
+{
+    // 一般方法:無傳回
+    void MethodA()
+    {
+        
+    }
+
+    // 有傳回方法
+    int MethodB()
+    {
+        return 10;
+    }
+
+    // 協程方法
+    // 1.傳回類型:IEnumerator
+    // 2.yield return 時間 new WaitForSeconds(秒數) null 一個影格的時間 
+    IEnumerator Test()
+    {
+        print("我是協程的第一行");
+        yield return new WaitForSeconds(2);
+        print("我是二秒後的程式"); 
+    }
+
+    private void Start()
+    {
+        // 呼叫協程
+        StartCoroutine(Test());
+        // 啟動協程( Big() )
+        StartCoroutine(Big());
+    }
+
+    public Transform cube;
+
+    private IEnumerator Big()
+    {
+        // 迴圈 for 0 ~ 10
+        for(int i= 0; i<10; i++)
+        { cube.localScale += Vector3.one;       // 尺寸 += 三維向量的 1
+        yield return new WaitForSeconds(0.5f);  // 等待半秒
+        }       
+    }
+
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("載入畫面")]
+    public GameObject panel;
+    [Header("載入進度")]
+    public Text textLoading;
+    [Header("載入吧條")]
+    public Image imgLoading;
+    [Header("提示文字")]
+    public Text textTip;
+
+    /// <summary> 怒關遊戲 </summary>
+    public void QuitGame()
+    {
+        Application.Quit(); // 應用程式.離開
+    }
+
+    /// <summary> 開始被玩 </summary>
+    public void StartGame()
+    {
+        StartCoroutine(Loading());  // 啟動協程
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator Loading()
+    {
+        panel.SetActive(true);
+        textTip.enabled = false;
+        AsyncOperation ao = SceneManager.LoadSceneAsync("GameStage01");    // 非同步載入資訊 = 場景管理器.非同步載入("場景名稱")
+        ao.allowSceneActivation = false;                        // 載入資訊.允許自動載入 = 否
+
+        // ao.isDone == true    簡寫 ao.isDone
+        // ao.isDone == false   簡寫 !ao.isDone
+        // 當 載入資訊.完成 == false -> 尚未載入完成時 執行迴圈
+        while (!ao.isDone)
+        {
+            // progress 載入場景的進度值為 0 - 1 ,如果 allow 設定為 false 會卡在 0.9
+            // ToString("F小數點點位數") : 小數點兩位 F2  小數點零位 F0 - F 大小寫皆可
+            textLoading.text = "載入進度 : " + (ao.progress / 0.9f * 100).ToString("F2") + "%"; // 載入文字 = "載入進度" + ao.進度*100 + "%"
+            imgLoading.fillAmount = ao.progress / 0.9f;                        // 載入吧條 = ao.進度 
+            yield return null;
+
+            if(ao.progress == 0.9f)     // 如果 ao.進度 等於 0.9
+            {
+                textTip.enabled = true; // 提示文字.啟動 = 是 - 顯示提示文字
+
+                if (Input.anyKeyDown)   ao.allowSceneActivation = true; // 如果按下任意鍵 允許自動載入
+            }  
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
